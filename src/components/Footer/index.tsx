cfd8d4ffa71e0b4dcd8f10e762107feb2abc7feb
MiguelBritoBarbosa <107328582+MiguelBritoBarbosa@users.noreply.github.com>
2023-11-02 17:02:51 -0300

    Fix Footer Container

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,10 @@
 import { theme } from '@/styles/theme';
-import { ContainerHeader } from './styled';
+import { ContainerFooter } from './styled';
 
 export const Footer = () => {
     return (
-        <ContainerHeader className="p-3 text-center tw-mt-96">
+        <ContainerFooter className="p-3 text-center tw-mt-96">
             <h1 style={{ color: theme.colors.primary }}>Footer!</h1>
-        </ContainerHeader>
+        </ContainerFooter>
     );
 };
--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -1,6 +1,6 @@
 "use client";
 import styled from "styled-components";
 
-export const ContainerHeader = styled.header`
+export const ContainerFooter = styled.footer`
     background-color: aliceblue;
 `;
